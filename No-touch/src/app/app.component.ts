77ec959e247b5758578103737d3cbf8400bc0c56
Juan Andres <jaarbelaezquintero@gmail.com>
2023-10-12 12:00:09 -0500

    login

--- No-touch/src/app/app.component.ts
+++ No-touch/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private http:HttpClient
+  ) {
+  }
+
   title = 'No-touch';
+
+   httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  loginPost(){
+    const {username, password} = this.formLogin.value;
+
+    const body = {"username":username,"password":password}
+
+    this.http.post('http://xxxxxx', body, this.httpOptions).subscribe({
+      next:(response) =>{console.log(response)}
+    })
+  }
+
 }
